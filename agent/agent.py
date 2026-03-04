c75141909e095a44c36dd55af308f5381c8d6546
amelia-haa <ameliaha1810@gmail.com>
2026-03-04 00:20:18 -0500

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,11 +1,42 @@
 from google.adk.agents import LlmAgent
-from dotenv import load_dotenv
+from google.adk.tools import agent_tool
+from google.adk.tools.google_search_tool import GoogleSearchTool
+from google.adk.tools import url_context
 
-load_dotenv()
-
-assistant = LlmAgent(
-    name="supply_chain_agent",
-    model="gemini-2.5-flash",
-    description="AI Supply Chain Resilience Co-Pilot",
-    instruction="You are an AI operations co-pilot for mid-market manufacturers."
+supply_chain_agent__google_search_agent = LlmAgent(
+  name='supply_chain_agent__google_search_agent',
+  model='gemini-2.5-flash',
+  description=(
+      'Agent specialized in performing Google searches.'
+  ),
+  sub_agents=[],
+  instruction='Use the GoogleSearchTool to find information on the web.',
+  tools=[
+    GoogleSearchTool()
+  ],
+)
+supply_chain_agent__url_context_agent = LlmAgent(
+  name='supply_chain_agent__url_context_agent',
+  model='gemini-2.5-flash',
+  description=(
+      'Agent specialized in fetching content from URLs.'
+  ),
+  sub_agents=[],
+  instruction='Use the UrlContextTool to retrieve content from provided URLs.',
+  tools=[
+    url_context
+  ],
+)
+root_agent = LlmAgent(
+  name='supply_chain_agent_',
+  model='gemini-2.5-flash',
+  description=(
+      'Agent to help interact with my data.'
+  ),
+  sub_agents=[],
+  instruction='',
+  tools=[
+    agent_tool.AgentTool(agent=supply_chain_agent__google_search_agent),
+    agent_tool.AgentTool(agent=supply_chain_agent__url_context_agent)
+  ],
 )
